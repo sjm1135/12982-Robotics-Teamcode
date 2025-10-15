1799a37514cb7b1ee93fe7aa7cce93c3126410c8
zackrosenfeld <zackrosenfeld3@gmail.com>
2025-10-15 15:24:33 -0500

    edited RPMTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestModes/ConceptAprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestModes/ConceptAprilTag.java
@@ -149,8 +149,7 @@ public class ConceptAprilTag extends LinearOpMode {
         // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
         // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
         // Note: Decimation can be changed on-the-fly to adapt during a match.
-        aprilTag.setDecimation(1);
-        aprilTag.setDecimation(1);
+        aprilTag.setDecimation(3);
 
         // Create the vision portal by using a builder.
         VisionPortal.Builder builder = new VisionPortal.Builder();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestModes/RPMTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestModes/RPMTest.java
@@ -10,9 +10,9 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "RPM_Test", group = "test_modes")
 @Config
@@ -22,12 +22,15 @@ public class RPMTest extends LinearOpMode {
                                                           // All calculations for control are done
                                                           // In terms of RPM
     public static double targetRPM = 0;
+    private double power = 0;
     public static class Params {
-        public double kp = 0.001;
+        public double kp = 0.01;
         public double ki = 0;
         public double kd = 0;
         public double imax = 1;
     }
+
+    ElapsedTime timer = new ElapsedTime();
     Params params = new Params();
     PID controller = new PID(params.kp, params.ki, params.kd, params.imax);
 
@@ -41,7 +44,10 @@ public class RPMTest extends LinearOpMode {
         launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         launcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+
         waitForStart();
+
+        timer.reset();
 
         while(opModeIsActive()) {
 
@@ -49,7 +55,7 @@ public class RPMTest extends LinearOpMode {
             double RPM = launcher.getVelocity() / MOTOR_TICKS_PER_REVOLUTION * 60;
 
             // Plugging position into PID controller
-            double power = controller.PIDControl(RPM, targetRPM);
+            power += controller.PIDControl(RPM, targetRPM) * timer.seconds();
 
             // Limiting motor power to 1
             if (power > 1) {
@@ -66,7 +72,7 @@ public class RPMTest extends LinearOpMode {
             /*
              * TUNING
              *
-             * The following code sends the current motor velocity compared to the target velocity
+             * The following code sends the current motor velocity and the target velocity
              * to ftc dashboard
              *
              * graphing velocity and target velocity should give a good idea of how well
@@ -77,6 +83,8 @@ public class RPMTest extends LinearOpMode {
             packet.put("Velocity (RPM): ", RPM);
             packet.put("Target Velocity (RPM): ", targetRPM);
             dashboard.sendTelemetryPacket(packet);
+
+            timer.reset();
 
         }
     }
