f3fa60e669efe6e67d3a25bb23180ec4496443b5
sjm1135 <27maysj@rbhs208.net>
2025-11-19 15:58:49 -0600

    teleop update that I screwed up earlier hopefully this works keep in mind none of this has been tested as of 11/19 -sophie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2026.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2026.java
@@ -1,95 +1,124 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-@TeleOp(name = "TeleOp2026")
-public class TeleOp2026 extends OpMode {
+ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+ import com.qualcomm.robotcore.hardware.CRServo;
+ import com.qualcomm.robotcore.hardware.DcMotor;
+ import com.qualcomm.robotcore.hardware.DcMotorEx;
+ import com.qualcomm.robotcore.hardware.DcMotorSimple;
+ import com.qualcomm.robotcore.hardware.Servo;
+ @TeleOp(name = "TeleOp2026")
+ public class TeleOp2026 extends OpMode {
 
 
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
-    private DcMotorEx launchMotor;
-    private CRServo leftLaunchServo;
-    private CRServo rightLaunchServo;
-    private double speedControl = 0.4;
-    private final double WHEEL_WHEEL_DIAMETER = 4.094; //in inches
-    private final double TICKS_PER_REVOLUTION_WHEELS = 2000; //for wheels
-    private final double FLYWHEEL_DIAMETER = 3.78;
-    private final double FLYWHEEL_TICKS_PER_REVOLUTION = 28;
-    private final double TICK_PER_SECOND = FLYWHEEL_TICKS_PER_REVOLUTION * 100; //ticks*rpm/60
-    boolean toggle = false;
-    private double max = 0;
-    @Override
-    public void init(){
-        frontLeft = hardwareMap.get(DcMotor.class, "leftFrontDrive");
-        frontRight = hardwareMap.get(DcMotor.class, "rightFrontDrive");
-        backLeft = hardwareMap.get(DcMotor.class, "leftBackDrive");
-        backRight = hardwareMap.get(DcMotor.class, "rightBackDrive");
-        launchMotor = hardwareMap.get(DcMotorEx.class, "launchLaunchMotor");
-        leftLaunchServo = hardwareMap.get(CRServo.class, "launchLeftServo");
-        rightLaunchServo = hardwareMap.get(CRServo.class, "launchRightServo");
-        telemetry.addData("Hardware: ", "Initialized");
-    }
-    @Override
-    public void loop() {
-        launchMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        launchMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftLaunchServo.setDirection(DcMotorSimple.Direction.REVERSE);
-        //basic driving
-        double y = -gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x;
-        double rx = gamepad1.right_stick_x;
+     private DcMotor frontLeft;
+     private DcMotor frontRight;
+     private DcMotor backLeft;
+     private DcMotor backRight;
+     private DcMotorEx launchMotor;
+     private CRServo leftLaunchServo;
+     private CRServo rightLaunchServo;
+     private Servo leftPosControl;
+     private Servo rightPosControl;
+     private double speedControl = 0.4;
+     private double TARGET_RPM = 3000;
+     private final double WHEEL_WHEEL_DIAMETER = 4.094; //in inches
+     private final double TICKS_PER_REVOLUTION_WHEELS = 2000; //for wheels
+     private final double FLYWHEEL_DIAMETER = 3.78;
+     private final double FLYWHEEL_TICKS_PER_REVOLUTION = 28;
+     boolean toggle = false;
+     private double max = 0;
+     @Override
+     public void init(){
+         frontLeft = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+         frontRight = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+         backLeft = hardwareMap.get(DcMotor.class, "leftBackDrive");
+         backRight = hardwareMap.get(DcMotor.class, "rightBackDrive");
+         launchMotor = hardwareMap.get(DcMotorEx.class, "launchLaunchMotor");
+         leftLaunchServo = hardwareMap.get(CRServo.class, "launchLeftServo");
+         rightLaunchServo = hardwareMap.get(CRServo.class, "launchRightServo");
+         leftPosControl = hardwareMap.get(Servo.class, "leftPos");
+         rightPosControl = hardwareMap.get(Servo.class, "rightPos");
+         telemetry.addData("Hardware: ", "Initialized");
 
-        double frontLeftPower  = y + x + rx;
-        double frontRightPower = y - x - rx;
-        double backLeftPower   = y - x + rx;
-        double backRightPower  = y + x - rx;
+         launchMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         launchMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+         leftLaunchServo.setDirection(DcMotorSimple.Direction.REVERSE);
+     }
+     @Override
+     public void loop() {
+         //basic driving
+         double y = -gamepad1.left_stick_y;
+         double x = gamepad1.left_stick_x;
+         double rx = gamepad1.right_stick_x;
 
-        max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
-        max = Math.max(max, Math.abs(backLeftPower));
-        max = Math.max(max, Math.abs(backRightPower));
+         double frontLeftPower  = y + x + rx;
+         double frontRightPower = y - x - rx;
+         double backLeftPower   = y - x + rx;
+         double backRightPower  = y + x - rx;
 
-        if (max > 1.0) {
-            frontLeftPower  /= max;
-            frontRightPower /= max;
-            backLeftPower   /= max;
-            backRightPower  /= max;
-        }
+         max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+         max = Math.max(max, Math.abs(backLeftPower));
+         max = Math.max(max, Math.abs(backRightPower));
 
-        frontLeft.setPower(frontLeftPower);
-        backLeft.setPower(backLeftPower);
-        frontRight.setPower(frontRightPower);
-        backRight.setPower(backRightPower);
+         if (max > 1.0) {
+             frontLeftPower  /= max;
+             frontRightPower /= max;
+             backLeftPower   /= max;
+             backRightPower  /= max;
+         }
 
-        //mounted mechanism mechanisms
-        //TODO: fix this by making the trigger toggle a boolean and the velocity dependent on said boolean
+         frontLeft.setPower(frontLeftPower);
+         backLeft.setPower(backLeftPower);
+         frontRight.setPower(frontRightPower);
+         backRight.setPower(backRightPower);
+         //mounted mechanism mechanisms
 
-        if (gamepad2.left_trigger > 0) //start flywheel
-        {
-            toggle = true;
-        }
-        else if (gamepad2.right_trigger > 0) //stop flywheel
-        {
-            toggle = false;
-        }
-        if (toggle) {
-            launchMotor.setVelocity(TICK_PER_SECOND);
-        }
-        if (gamepad2.square == true)
-        {
-            //TODO: put in command for servos to release ball
-            leftLaunchServo.setPower(.5);
-            rightLaunchServo.setPower(.5);
-        }
-        else {
-            leftLaunchServo.setPower(0);
-            rightLaunchServo.setPower(0);
-        }
-    }
-}
+         if (gamepad2.left_trigger > 0) //start flywheel
+         {
+             toggle = true;
+         }
+         else if (gamepad2.right_trigger > 0) //stop flywheel
+         {
+             toggle = false;
+         }
+
+         double TICKS_PER_SECOND = FLYWHEEL_TICKS_PER_REVOLUTION * TARGET_RPM / 60;
+
+         if (toggle) {
+             launchMotor.setVelocity(TICKS_PER_SECOND);
+         }
+         else {
+             launchMotor.setVelocity(0);
+         }
+         double servoPower = gamepad2.left_stick_y;
+         leftLaunchServo.setPower(servoPower);
+         rightLaunchServo.setPower(servoPower);
+
+         //emergency brake to avoid Stanley Time
+         if (gamepad1.b) {
+             frontLeft.setPower(0);
+             frontRight.setPower(0);
+             backLeft.setPower(0);
+             backRight.setPower(0);
+             launchMotor.setVelocity(0);
+             leftLaunchServo.setPower(0);
+             rightLaunchServo.setPower(0);
+         }
+         double servoPos = .8;
+         if (gamepad1.left_trigger > 0)
+         {
+             leftPosControl.setPosition(servoPos);
+             rightPosControl.setPosition(1-servoPos);
+             TARGET_RPM = 3700;
+         }
+         else if (gamepad1.right_trigger > 0)
+         {
+             leftPosControl.setPosition(0);
+             rightPosControl.setPosition(1);
+             TARGET_RPM = 3000;
+         }
+         telemetry.addData("rightPosition: ", rightPosControl.getPosition());
+         telemetry.addData("leftPosition: ", leftPosControl.getPosition());
+         telemetry.update();
+     }
+ }
